42d0f8f67a269fb42b65d634df87103412a52b34
GoodOne <89701985+GoodOne120@users.noreply.github.com>
2022-07-15 14:50:40 +0300

    Updated "Start.bat"

--- Start.bat
+++ Start.bat
@@ -1,3 +1,7 @@
+  node -v 2> Nul
+if "%errorlevel%" == "9009" (
+  echo "Node.JS is not installed on your system!"
+) else (
 if exist node_modules\axios\ (
   cls
   title CFX.re-IP-Finder
@@ -8,4 +12,5 @@ if exist node_modules\axios\ (
   cls
   title CFX.re-IP-Finder
   node main.js
+)
 )
