4a0fea9c82a8c87dc86aaf9f80f1b0aa5d92556c
GoodOne <89701985+GoodOne120@users.noreply.github.com>
2023-01-15 14:54:28 +0200

    Updated "main.js"

--- main.js
+++ main.js
@@ -9,55 +9,45 @@ console.log("\x1b[0m");
 const readline = require('readline');
 const axios = require('axios');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-var repeat = function () {
-rl.question("Please enter server's CFX.re IP address (cfx.re/join/XXXXXX or XXXXXX): ", (url) => {
+function repeat() {
+    rl.question("Please enter server's CFX.re IP address (cfx.re/join/XXXXXX or XXXXXX): ", async (url) => {
 
-if (url.startsWith("cfx.re/join/")) {
-  
-  var request = "https://" + url
+        if (url.startsWith("cfx.re/join/")) {
 
-}
+            var request = "https://" + url
 
-else if (url.startsWith("https://cfx.re/join/")) {
-  
-  var request = url
-  var url = url.replace("https://", "")
-  
-} 
+        } else if (url.startsWith("https://cfx.re/join/")) {
 
-else {
-  
-  var request = "https://cfx.re/join/" + url
-  var url = "cfx.re/join/" + url;
+            var request = url
+            var url = url.replace("https://", "")
 
-}
+        } else {
 
-  async function storeheader() {
+            var request = "https://cfx.re/join/" + url
+            var url = "cfx.re/join/" + url;
 
-try {
+        }
 
-  var response = (await axios.get(request)).headers["x-citizenfx-url"].replace("http://", "").replace("/", "");
+        try {
 
-} catch (error) {
+            var response = (await axios.get(request)).headers["x-citizenfx-url"].replace("http://", "").replace("/", "");
 
-  var response = "Server was not found! Check server's CFX.re IP address or server might be offline!"
+        } catch (error) {
 
-}
+            var response = "Server was not found! Check server's CFX.re IP address or server might be offline!"
 
-  console.log(`\nCFX.re IP: \x1b[36m${url}`);
-  console.log(`\x1b[0mIP address: \x1b[36m${response}\x1b[0m\n`);
-  console.log(`\x1b[32mNote: Remember that some servers have their IP hidden, but there is another way to get it. Also make sure that server is online!\n`)
-  console.log(`\x1b[33mYou can contact me on Discord, just be kind - GoodOne#4446.`);
-  console.log(`\x1b[33mFeel free to request features and report bugs. I will fix them or implement features as fast as I can. :)\x1b[0m\n`);
-  repeat();
+        }
 
-}
-
-storeheader()
-});
+        console.log(`\nCFX.re IP: \x1b[36m${url}`);
+        console.log(`\x1b[0mIP address: \x1b[36m${response}\x1b[0m\n`);
+        console.log(`\x1b[32mNote: Remember that some servers have their IP hidden, but there is another way to get it. Also make sure that server is online!\n`)
+        console.log(`\x1b[33mYou can contact me on Discord, just be kind - GoodOne#4446.`);
+        console.log(`\x1b[33mFeel free to request features and report bugs. I will fix them or implement features as fast as I can. :)\x1b[0m\n`);
+        repeat();
+    });
 };
 repeat();
