f8ad695eebf4de6188b0709d71f3b5d2502c1a28
GoodOne <89701985+GoodOne120@users.noreply.github.com>
2023-01-15 14:54:13 +0200

    Updated "Start.sh"
    Checks if Node.js is installed

--- Start.sh
+++ Start.sh
@@ -1,1 +1,5 @@
-[ -d "node_modules/axios/" ] && clear && node main.js || npm i axios && npm audit fix && clear && node main.js
+if which node >/dev/null; then
+    [ -d "node_modules/axios/" ] && clear && node main.js || npm i axios && npm audit fix && clear && node main.js
+else
+    echo "Node.JS is not installed on your system!"
+fi
